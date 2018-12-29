9f517919e5594fc49c6e9a45144718c30bb5765b
matsumoto-taichi <skkxt541@yahoo.co.jp>
2018-12-29 17:41:11 +0900

    fix

--- app/controllers/line_controller.rb
+++ app/controllers/line_controller.rb
@@ -65,9 +65,7 @@ class LineController < ApplicationController
     body = request.body.read
     signature = request.env['HTTP_X_LINE_SIGNATURE']
     unless client.validate_signature(body, signature)
-      error 400 do
-        'Bad Request'
-      end
+      error 400 do 'Bad Request' end
     end
   end
 end
